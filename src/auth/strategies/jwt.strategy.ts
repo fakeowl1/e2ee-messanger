3d126aff64762f7227abb35e8a5fabd4f9983c98
fakeowl1 <ukolov.roman@lll.kpi.ua>
2026-07-31 12:37:05 +0300

    feat: add jwt strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+type JwtPayload = {
+  sub: number;
+  email: string;
+};
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    const jwtSecret = configService.getOrThrow<string>('jwt.secret');
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtSecret,
+    });
+  }
+
+  validate(payload: JwtPayload): JwtPayload {
+    return payload;
+  }
+}
