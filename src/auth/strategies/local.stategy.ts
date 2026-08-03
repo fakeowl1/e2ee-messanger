3082008a97b9dba61f11929c9c1e97a0e3f4f31e
fakeowl1 <ukolov.roman@lll.kpi.ua>
2026-08-03 13:52:23 +0300

    ref: update type for local strategy validate method

--- src/auth/strategies/local.stategy.ts
+++ src/auth/strategies/local.stategy.ts
@@ -2,6 +2,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from 'src/user/user.repository';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -9,7 +10,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     super({ usernameField: 'email' });
   }
 
-  async validate(email: string, password: string) {
+  async validate(email: string, password: string): Promise<User | undefined> {
     const user = await this.authService.validateUser({ email, password });
     if (!user) throw new UnauthorizedException('Invalid credentials');
     return user;
