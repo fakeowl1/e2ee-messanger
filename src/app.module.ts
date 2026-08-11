4b6011cd05a97412ab6696c3068bc44a9eb027d4
fakeowl1 <ukolov.roman@lll.kpi.ua>
2026-08-11 20:54:17 +0300

    fix: delete unnecessary import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
